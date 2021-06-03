684f31a9c42cc83a1c74aa6b84bd9947aa413b67
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-03 16:06:35 -0500

    Add branching to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -2,45 +2,34 @@
 function add(number1, number2){
 	return number1 + number2;
 }
-function sub(number1, number2){
+function subtract(number1, number2){
 	return number1 - number2;
 }
-function mul(number1, number2){
+function multiply(number1, number2){
 	return number1 * number2;
 }
-function div(number1, number2){
+function divide(number1, number2){
 	return number1 / number2;
 }
 
 //Below is user interface logic
 
 $(document).ready(function() {
-  $("form#add").submit(function(event){
+  $("form#calculator").submit(function(event){
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
-  })
-  $("form#sub").submit(function(event){
-    event.preventDefault();
-    const number1 = parseInt($("#sub1").val());
-    const number2 = parseInt($("#sub2").val());
-    const result = sub(number1, number2);
-    $("#output").text(result);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+      if (operator === "add") {
+        result = add(number1, number2);
+      } else if (operator === "subtract") {
+        result = subtract(number1, number2);
+      } else if (operator === "multiply") {
+        result = multiply(number1, number2);
+      } else if (operator === "divide") {
+        result = divide(number1, number2);
+      }
+      $("#output").text(result);
+    });
   });
-  $("form#mul").submit(function(event){
-    event.preventDefault();
-    const number1 = parseInt($("#mul1").val());
-    const number2 = parseInt($("#mul2").val());
-    const result = mul(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#div").submit(function(event){
-    event.preventDefault();
-    const number1 = parseInt($("#div1").val());
-    const number2 = parseInt($("#div2").val());
-    const result = div(number1, number2);
-    $("#output").text(result);
-  });
-});
